bbb4eea4e2f623d743c9a958ad775877748d76d8
Имя Фамилия <ehsonboboev7@gmail.com>
2024-12-06 00:06:22 +0700

    topappbar

---
+++ app/src/main/java/com/example/pr26_boboev__e/MainActivity.kt
@@ -0,0 +1,71 @@
+package com.example.pr26_boboev__e
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.pr26_boboev__e.ui.theme.Pr26_Boboev__ETheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Pr26_Boboev__ETheme {
+                mainscreen()
+            }
+        }
+    }
+}
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun mainscreen() {
+    TopAppBar(
+        title = {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                contentAlignment = Alignment.Center // Центровка текста
+            ) {
+                Text(
+                    text = "Wallet",
+                    fontSize = 22.sp,
+                    color = colorResource(R.color.text), // Белый цвет текста
+                )
+            }
+        },
+        // Если хотите изменить цвет фона AppBar:
+        colors = androidx.compose.material3.TopAppBarDefaults.topAppBarColors(
+            containerColor = androidx.compose.ui.graphics.Color.White // Пример цвета фона
+        ),
+
+        modifier = Modifier.shadow(4.dp) // Тень текста
+        ,
+
+
+    )
+}
